ede7cd3a40947e695b8c40d76dc52d2d3cb6d42d
mochenya <2827075398@qq.com>
2023-09-19 20:20:31 +0800

    add crossterm package

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,23 @@
+use std::fs;
+use std::io::{stdout, Write};
 use clap::Parser;
+use crossterm::{cursor, execute, QueueableCommand};
+use crossterm::terminal::{Clear, ClearType};
 
 mod args;
 
 pub fn run() {
     let args = args::Args::parse();
-    let name = args.file_path.unwrap_or_else(|| "world".to_string());
-    println!("file path : {}!", name);
+    let file_path = args.file_path.unwrap_or_else(|| "world".to_string());
+    println!("file path : {}", file_path);
+    let file_content = fs::read_to_string(file_path)
+        .expect("Something went wrong reading the file");
+    println!("file content : \n{}", file_content);
+
+    let mut stdout = stdout();
+    // 清空屏幕
+    execute!(stdout, Clear(ClearType::All)).expect("Failed to clear screen");
+    stdout.queue(cursor::MoveTo(0, 0)).expect("Failed to move cursor");
+    println!("Hello, world!");
+    stdout.flush().unwrap();
 }
