111d5afaf11ed269c5dc77e99bffb846ad8ccbae
mochenya <2827075398@qq.com>
2023-09-25 09:18:49 +0800

    add insert mode delete char

--- src/editor/mod.rs
+++ src/editor/mod.rs
@@ -4,7 +4,7 @@ pub mod render;
 
 use crate::args;
 use crate::editor::render::Render;
-use crate::files::reader::FileContent;
+use crate::files::reader::{FileContent, Line};
 use clap::Parser;
 use crossterm::cursor;
 use crossterm::execute;
@@ -87,25 +87,24 @@ impl Editor {
     pub(crate) fn add_content_char(&mut self, char: char) {
         let mut stdout = stdout();
         let (cursor_width, cursor_height) = cursor::position().unwrap();
-        // 获取当前行数
-        let current_line_number = self.start_line + cursor_height as usize;
+        let line_number_len = self.line_number_len;
         // 获取当前行信息
-        let mut current_line_info = match self.file_content.content.get_mut(current_line_number) {
+        let current_line_info = match Self::get_current_line_info(self) {
             Some(line_info) => line_info,
             None => {
                 // 获取最后一行信息
-                let last_line_info = self.file_content.content.last().unwrap();
+                let _last_line_info = self.file_content.content.last().unwrap();
                 return;
             }
         };
         // 添加字符
         current_line_info
             .text
-            .insert(cursor_width as usize - self.line_number_len - 1, char);
+            .insert(cursor_width as usize - line_number_len - 1, char);
         // 更新当前行信息
         Render::render_content_line(
             &mut stdout,
-            self.line_number_len,
+            line_number_len,
             cursor_height,
             &current_line_info,
         );
@@ -113,7 +112,35 @@ impl Editor {
         self.move_right();
     }
 
-    pub(crate) fn delete_content_char(&mut self) {}
+    pub(crate) fn delete_content_char(&mut self) {
+        let mut stdout = stdout();
+        let (cursor_width, cursor_height) = cursor::position().unwrap();
+        let line_number_len = self.line_number_len;
+        // 检查光标是否处于行首
+        if cursor_width as usize == line_number_len + 1 {
+            return;
+        }
+        // 获取当前行信息
+        let current_line_info = match Self::get_current_line_info(self) {
+            Some(line_info) => line_info,
+            None => {
+                return;
+            }
+        };
+        // 删除字符
+        current_line_info
+            .text
+            .remove(cursor_width as usize - line_number_len - 2);
+        // 更新当前行信息
+        Render::render_content_line(
+            &mut stdout,
+            line_number_len,
+            cursor_height,
+            &current_line_info,
+        );
+        // 更新光标位置
+        self.move_left();
+    }
 
     pub(crate) fn line_feed(&mut self) {}
 }
@@ -121,5 +148,13 @@ impl Editor {
 impl Editor {
     pub(crate) fn reset_cursor() {
         execute!(stdout(), cursor::MoveTo(0, 0)).unwrap();
+    }
+
+    pub(crate) fn get_current_line_info(editor: &mut Editor) -> Option<&mut Line> {
+        let (_cursor_width, cursor_height) = cursor::position().unwrap();
+        // 获取当前行数
+        let current_line_number = editor.start_line + cursor_height as usize;
+        // 获取当前行信息
+        editor.file_content.content.get_mut(current_line_number)
     }
 }
